61b7fdf2a865565cad57ab5bb058b1cb5a54d6c9
Piero Coronado Flores <pacoronado01@gmail.com>
2026-01-22 16:13:40 -0500

    Initial commit

---
+++ src/routes/__root.tsx
@@ -0,0 +1,25 @@
+import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
+import { TanStackDevtools } from '@tanstack/react-devtools'
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+
+export const Route = createRootRoute({
+  component: () => (
+    <>
+      <Outlet />
+      <ReactQueryDevtools buttonPosition="top-right" />
+      <TanStackDevtools
+        config={{
+          position: 'bottom-right',
+        }}
+        plugins={[
+          {
+            name: 'Tanstack Router',
+            render: <TanStackRouterDevtoolsPanel />,
+          },
+        ]}
+      />
+    </>
+  ),
+})
