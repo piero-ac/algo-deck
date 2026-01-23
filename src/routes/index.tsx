a62c8b9fd51bc0d18a13c2d49e18782a115a931a
Piero Coronado Flores <pacoronado01@gmail.com>
2026-01-23 18:43:04 -0500

    Add ProblemHistoryTable

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { ReviewsCard } from "@/components/reviews-card";
 import { ProblemsCard } from "@/components/problems-card";
+import { ProblemHistoryTable } from "@/components/problem-history-table";
 
 export const Route = createFileRoute("/")({
 	component: App,
@@ -8,13 +9,16 @@ export const Route = createFileRoute("/")({
 
 function App() {
 	return (
-		<div className="w-full p-5">
+		<div className="w-full p-5 flex flex-col gap-4 items-center">
 			{/* Reviews and Add Problems Cards */}
-			<div className="flex gap-5">
+			<div className="flex justify-center gap-5">
 				<ProblemsCard />
 				<ReviewsCard />
 			</div>
 			{/* Recent History Table */}
+			<div className="w-200">
+				<ProblemHistoryTable />
+			</div>
 		</div>
 	);
 }
