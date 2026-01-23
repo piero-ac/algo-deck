d11d4d99da4c867a9b282cedc20ffbe3c2d5f21a
Piero Coronado Flores <pacoronado01@gmail.com>
2026-01-23 13:37:28 -0500

    Add problems and reviews cards

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,39 +1,20 @@
-import { createFileRoute } from '@tanstack/react-router'
-import logo from '../logo.svg'
+import { createFileRoute } from "@tanstack/react-router";
+import { ReviewsCard } from "@/components/reviews-card";
+import { ProblemsCard } from "@/components/problems-card";
 
-export const Route = createFileRoute('/')({
-  component: App,
-})
+export const Route = createFileRoute("/")({
+	component: App,
+});
 
 function App() {
-  return (
-    <div className="text-center">
-      <header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
-        <img
-          src={logo}
-          className="h-[40vmin] pointer-events-none animate-[spin_20s_linear_infinite]"
-          alt="logo"
-        />
-        <p>
-          Edit <code>src/routes/index.tsx</code> and save to reload.
-        </p>
-        <a
-          className="text-[#61dafb] hover:underline"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <a
-          className="text-[#61dafb] hover:underline"
-          href="https://tanstack.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn TanStack
-        </a>
-      </header>
-    </div>
-  )
+	return (
+		<div className="w-full p-5">
+			{/* Reviews and Add Problems Cards */}
+			<div className="flex gap-5">
+				<ProblemsCard />
+				<ReviewsCard />
+			</div>
+			{/* Recent History Table */}
+		</div>
+	);
 }
