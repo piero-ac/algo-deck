9c387633bdb86c7e11d1cbe4c4c88219f5a1b885
Piero Coronado Flores <pacoronado01@gmail.com>
2026-01-23 13:35:06 -0500

    Create initial sidebar component

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,60 @@
+import { Home, Target, History } from "lucide-react";
+
+import {
+	Sidebar,
+	SidebarContent,
+	SidebarGroup,
+	SidebarGroupContent,
+	SidebarGroupLabel,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+import { Link } from "@tanstack/react-router";
+
+// Menu items.
+const items = [
+	{
+		title: "Home",
+		url: "/",
+		icon: Home,
+	},
+	{
+		title: "Reviews",
+		url: "/reviews",
+		icon: Target,
+	},
+	{
+		title: "History",
+		url: "/history",
+		icon: History,
+	},
+];
+
+export function AppSidebar() {
+	return (
+		<Sidebar>
+			<SidebarContent>
+				{/* Application Menu */}
+				<SidebarGroup>
+					<SidebarGroupLabel>AlgoDeck</SidebarGroupLabel>
+					<SidebarGroupContent>
+						<SidebarMenu>
+							{items.map((item) => (
+								<SidebarMenuItem key={item.title}>
+									<SidebarMenuButton asChild>
+										<Link to={item.url} className="flex items-center space-x-2">
+											<item.icon />
+											<span>{item.title}</span>
+										</Link>
+									</SidebarMenuButton>
+								</SidebarMenuItem>
+							))}
+						</SidebarMenu>
+					</SidebarGroupContent>
+				</SidebarGroup>
+			</SidebarContent>
+		</Sidebar>
+	);
+}
