107c38ebe8bc3a42443af5f21c493ff729b5ea37
Piero Coronado Flores <pacoronado01@gmail.com>
2026-01-27 09:11:26 -0500

    Use context to get problems added to lessons

--- src/routes/lessons.tsx
+++ src/routes/lessons.tsx
@@ -1,17 +1,30 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { useLessonsQueue } from "@/context/lessonQueue";
 
 export const Route = createFileRoute("/lessons")({
 	component: RouteComponent,
-	loader: ({ context }) => context.lessonsQueue,
 });
 
 function RouteComponent() {
-	const lessons = Route.useLoaderData();
-	console.log(lessons.length);
+	const {
+		queue,
+		// removeNext,
+		// removeFromQueue
+	} = useLessonsQueue();
+
+	console.log(queue.length);
 	return (
 		<div>
 			Hello "/lessons"!
-			{lessons.length === 0 && <p>Go to problems page to add lessons</p>}
+			{queue.length === 0 && <p>Go to problems page to add lessons</p>}
+			{queue.length > 0 &&
+				queue.map((lesson) => {
+					return (
+						<div key={lesson.problem_number}>
+							{lesson.problem_title} {lesson.problem_difficulty}
+						</div>
+					);
+				})}
 		</div>
 	);
 }
