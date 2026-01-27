42132de4c7c7805dccd82124ed8c5838d8ff8007
Piero Coronado Flores <pacoronado01@gmail.com>
2026-01-27 09:09:59 -0500

    Use context add function

--- src/components/problem-item.tsx
+++ src/components/problem-item.tsx
@@ -6,16 +6,24 @@ import {
 	ItemDescription,
 	ItemTitle,
 } from "@/components/ui/item";
+import { useState } from "react";
 
 export function ProblemItem({
 	problemNumber,
 	problemTitle,
 	problemDifficulty,
+	addProblemToLesson,
 }: {
 	problemNumber: number;
 	problemTitle: string;
 	problemDifficulty: string;
+	addProblemToLesson: (problem: {
+		problem_number: number;
+		problem_title: string;
+		problem_difficulty: string;
+	}) => void;
 }) {
+	const [clicked, setClicked] = useState(false);
 	return (
 		<div className="flex w-full max-w-md flex-col gap-6">
 			<Item variant="outline">
@@ -28,8 +36,21 @@ export function ProblemItem({
 					</ItemDescription>
 				</ItemContent>
 				<ItemActions>
-					<Button variant="outline" size="sm">
-						Add to Reviews
+					<Button
+						variant="outline"
+						size="sm"
+						onClick={() => {
+							addProblemToLesson({
+								problem_number: problemNumber,
+								problem_title: problemTitle,
+								problem_difficulty: problemDifficulty,
+							});
+
+							setClicked(true);
+						}}
+						disabled={clicked}
+					>
+						Add to Lesson
 					</Button>
 				</ItemActions>
 			</Item>
