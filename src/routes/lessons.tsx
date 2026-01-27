7b17065d8cfb8ff5cacff85c6c7aa9956a92aa5a
Piero Coronado Flores <pacoronado01@gmail.com>
2026-01-27 13:43:23 -0500

    Design lessons queue page for submitting reviews

--- src/routes/lessons.tsx
+++ src/routes/lessons.tsx
@@ -1,30 +1,99 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { useLessonsQueue } from "@/context/lessonQueue";
+import { useSubmitReview } from "@/hooks/useSubmitReview";
+import { Button } from "@/components/ui/button";
+import { ButtonGroup, ButtonGroupText } from "@/components/ui/button-group";
 
 export const Route = createFileRoute("/lessons")({
 	component: RouteComponent,
 });
 
+type Rating = "again" | "good" | "easy" | "hard";
+
 function RouteComponent() {
 	const {
 		queue,
-		// removeNext,
+		removeNext,
 		// removeFromQueue
 	} = useLessonsQueue();
+	const submitReview = useSubmitReview();
 
-	console.log(queue.length);
+	const currentLesson = queue[0];
+
+	if (!currentLesson)
+		return <h2 className="text-3xl text-center mt-10">No lessons here.....</h2>;
+
+	function handleRate(rating: Rating) {
+		submitReview.mutate({
+			problemNumber: currentLesson.problem_number,
+			rating,
+		});
+	}
+
 	return (
-		<div>
-			Hello "/lessons"!
-			{queue.length === 0 && <p>Go to problems page to add lessons</p>}
-			{queue.length > 0 &&
-				queue.map((lesson) => {
-					return (
-						<div key={lesson.problem_number}>
-							{lesson.problem_title} {lesson.problem_difficulty}
-						</div>
-					);
-				})}
+		<div className="flex flex-col justify-center p-5">
+			<div className="text-center">
+				<h3>{queue.length} lesson's left</h3>
+				<h2 className="text-xl text-bold">
+					{currentLesson.problem_number}. {currentLesson.problem_title}
+				</h2>
+				{submitReview.isSuccess && (
+					<p className="text-xl text-green-500">
+						Next review on:{" "}
+						{new Date(submitReview.data.nextReviewAt).toLocaleDateString(
+							"en-US",
+						)}
+					</p>
+				)}
+			</div>
+
+			<div className="my-5 mx-auto">
+				<ButtonGroup>
+					<ButtonGroupText>Ratings</ButtonGroupText>
+					{["again", "easy", "good", "hard"].map((r) => (
+						<Button
+							className="capitalize"
+							key={r}
+							onClick={() => handleRate(r as Rating)}
+							disabled={submitReview.isPending || submitReview.isSuccess}
+						>
+							{r}
+						</Button>
+					))}
+				</ButtonGroup>
+			</div>
+			{submitReview.isPending && <p>Submitting review...</p>}
+			{submitReview.isError && (
+				<p>Error: {(submitReview.error as Error).message}</p>
+			)}
+
+			<div className="mx-auto flex gap-2">
+				<Button
+					onClick={() => {
+						submitReview.reset();
+						removeNext();
+					}}
+					disabled={
+						queue.length === 0 ||
+						!submitReview.isSuccess ||
+						submitReview.isPending
+					}
+					variant="default"
+				>
+					Next
+				</Button>
+				<Button
+					onClick={() => {
+						if (!confirm("Remove this problem from lessons?")) return;
+						submitReview.reset();
+						removeNext();
+					}}
+					disabled={submitReview.isPending || submitReview.isSuccess}
+					variant="destructive"
+				>
+					Skip This Lesson
+				</Button>
+			</div>
 		</div>
 	);
 }
